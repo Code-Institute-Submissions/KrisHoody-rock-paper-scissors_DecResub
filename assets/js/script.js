defbd2ff36a5db4439438a50721cb6acc7d83551
KrisHoody <hoodykris@gmail.com>
2021-10-07 11:28:13 +0000

    Add js script

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+let result_game = document.getElementById("result");
+let game_choices = {
+    'rock': {
+        'rock' : 'draw',
+        'scissors' : 'win',
+        'paper' : 'looe'
+    },
+    'scissors': {
+        'rock' : 'lose',
+        'scissors' : 'draw',
+        'paper' : 'win'
+    },
+    'paper': {
+        'rock' : 'win',
+        'scissors' : 'lose',
+        'paper' : 'draw'
+    }
+}
+

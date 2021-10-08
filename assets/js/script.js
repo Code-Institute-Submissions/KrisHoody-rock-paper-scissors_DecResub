801bbc8ac3589d871838b089958222945934b2b4
KrisHoody <hoodykris@gmail.com>
2021-10-08 10:39:48 +0000

    Add js script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,58 @@
-let result_game = document.getElementById("result");
-let game_choices = {
-    'rock': {
+
+let [comp_points, your_points] = [0, 0];
+let result_game = document.getElementById('result');
+let chooseRps = {
+    'rock' : {
         'rock' : 'draw',
-        'scissors' : 'win',
-        'paper' : 'looe'
+        'scissor' : 'win',
+        'paper' : 'lose'
     },
-    'scissors': {
+    'scissor' : {
         'rock' : 'lose',
-        'scissors' : 'draw',
+        'scissor' : 'draw',
         'paper' : 'win'
     },
-    'paper': {
+    'paper' : {
         'rock' : 'win',
-        'scissors' : 'lose',
+        'scissor' : 'lose',
         'paper' : 'draw'
     }
+
+}
+
+function playGame(input) {
+    var choices = ['rock', 'paper', 'scissors']
+    var num = Math.floor(Math.random() * 3);
+
+
+    document.getElementById('computer-choose').innerHTML =
+    `Computer choose <span> ${choices[num]} </span>`;
+
+    document.getElementById('you-choose').innerHTML = 
+    `You choose <span> ${input} </span>`;
+
+    let comp_choice = choices[num];
+
+    switch(playGame[input][comp_choice]) {
+        case 'win':
+            result_game.innerHTML = 'You win';
+            your_points++;
+            break;
+        case 'lose'  
+            result_game.innerHTML = 'You loose';
+            your_points++;
+            break; 
+            
+            default:
+            result_game.innerHTML = 'Tie';
+            break;   
+
+    }
+    document.getElementById('comp_points').innerHTML = 'comp_points';
+    document.getElementById('your_points').innerHTML = 'your_points';
+
+
+    
+    
 }
 
